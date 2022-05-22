c95e85b6a694e47b4192745a5d26bd11d4085213
Thanush A  A <73998493+aathanush@users.noreply.github.com>
2022-05-22 17:36:18 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -8,3 +8,12 @@ import pandas as pd
 #list of emotions in tess dataset
 emotions=['angry','disgust','fear','ps','happy','sad']
 
+
+def extract_feature(file_name, mfcc):
+    X, sample_rate = librosa.load(os.path.join(file_name), res_type='kaiser_fast')
+    if mfcc:
+        mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result=np.hstack((mfccs))
+        return result
+    else:
+        return None
